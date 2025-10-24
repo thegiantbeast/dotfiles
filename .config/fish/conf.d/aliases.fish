d221adffac4f62a3a76134d2201f2efa8b64d4c6
Ricardo Ferreira <me+github@ricardoferreira.eu>
2025-10-24 01:45:08 +0100

    chore: update dotfiles alias path

--- .config/fish/conf.d/aliases.fish
+++ .config/fish/conf.d/aliases.fish
@@ -3,7 +3,7 @@ alias fish-reload="source ~/.config/fish/config.fish"
 alias fish-config="code ~/.config/fish"
 alias git-config="code ~/.config/git"
 alias zellij-config="code ~/.config/zellij"
-alias dotfiles="code ~/Code/dotfiles"
+alias dotfiles="code ~/.dotfiles"
 
 alias tequity="zellij --layout tequity"
 alias momentus="zellij --layout momentus"
