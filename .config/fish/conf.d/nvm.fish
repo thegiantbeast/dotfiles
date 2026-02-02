7f485b8bf3ee98d0ffb9de97437a440c87ceeb71
Ricardo Ferreira <me+github@ricardoferreira.eu>
2026-02-02 13:24:23 +0000

    feat: add ghostty config and eager-load nvm on startup
    - Add ghostty terminal config with macos-option-as-alt and keybinds
    - Eager-load nvm default on fish startup for global npm binaries
    - Clean up fish_variables by removing default color settings
    - Add .gitignore for vim swap files

--- .config/fish/conf.d/nvm.fish
+++ .config/fish/conf.d/nvm.fish
@@ -1,2 +1,7 @@
 set -gx nvm_prefix (brew --prefix nvm)
 set -gx NVM_DIR (brew --prefix nvm)
+
+# Eager-load nvm default on shell startup so all global npm binaries are available
+if status is-interactive
+    nvm use default >/dev/null 2>&1
+end
